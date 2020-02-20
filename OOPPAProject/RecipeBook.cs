9b35d4ec8a18467367a53ed057c674ee661e50fd
jakabij <jakabistvanj@gmail.com>
2020-02-20 11:58:48 +0100

    raw RecipeBook (without separating the statements)

--- OOPPAProject/RecipeBook.cs
+++ OOPPAProject/RecipeBook.cs
@@ -29,7 +29,6 @@ namespace OOPPAProject
             {
                 int id = 0;
                 string userInput = Console.ReadLine();
-
                 bool needToCook;
                 if(bool.TryParse(userInput, out needToCook)){ }
                 else
@@ -68,15 +67,73 @@ namespace OOPPAProject
                     {
                         throw new Exception("ParseError");
                     }
+
+                    timeToPrepare = new TimeSpan(hours, minutes, seconds);
                 }
 
+                userInput = Console.ReadLine();
+                timeAfterSplit = userInput.Split(",");
+                List<string> listOfSpices = new List<string>();
+                listOfSpices.AddRange(timeAfterSplit);
 
-                List<string> listOfSpices;
-
-                food =new SecondMeal(id, nameOfFood, serveCold, listOfIngredients, )
+                food = new SecondMeal(id, nameOfFood, serveCold, listOfIngredients, needToCook, timeToPrepare, listOfSpices);
             }
 
-            return null;
+            else if(typeOfFood.ToLower().Equals("dessert"))
+            {
+                int id = 0;
+
+                string userInput = Console.ReadLine();
+                bool needToCook;
+                if (bool.TryParse(userInput, out needToCook)) { }
+                else
+                {
+                    throw new Exception("ParseError");
+                }
+
+                TimeSpan timeToPrepare; //00:00:00 format
+                userInput = Console.ReadLine();
+                string[] timeAfterSplit = userInput.Split(":");
+                if (timeAfterSplit.Length < 3 || timeAfterSplit.Length > 3)
+                {
+                    throw new Exception("ParseError");
+                }
+                else
+                {
+                    int hours = 0;
+                    int minutes = 1;
+                    int seconds = 2;
+
+                    if (int.TryParse(timeAfterSplit[hours], out hours)) { }
+                    else
+                    {
+                        throw new Exception("ParseError");
+                    }
+
+                    if (int.TryParse(timeAfterSplit[minutes], out minutes)) { }
+                    else
+                    {
+                        throw new Exception("ParseError");
+                    }
+
+                    if (int.TryParse(timeAfterSplit[seconds], out seconds)) { }
+                    else
+                    {
+                        throw new Exception("ParseError");
+                    }
+
+                    timeToPrepare = new TimeSpan(hours, minutes, seconds);
+                }
+
+                food = new Dessert(id, nameOfFood, serveCold, listOfIngredients, needToCook, timeToPrepare);
+            }
+            else
+            {
+                throw new Exception("NoFoodLikeThatException");
+            }
+
+
+            return food;
         }
 
         public void AddFood(Food food)
