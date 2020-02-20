8111fcf6156fc8d70730265e751600f1c6186d28
jakabij <jakabistvanj@gmail.com>
2020-02-20 11:32:19 +0100

    Modifing methods in RecipeBook class

--- OOPPAProject/RecipeBook.cs
+++ OOPPAProject/RecipeBook.cs
@@ -4,7 +4,84 @@ using System.Text;
 
 namespace OOPPAProject
 {
-    class RecipeBook
+    public class RecipeBook
     {
+        public int Id { get; set; }
+        public int Pages { get; set; }
+        public string NameOfBook { get; set; }
+        public List<Food> ListOfFoods { get; set; }
+
+        public RecipeBook()
+        {
+            ListOfFoods = new List<Food>();
+        }
+
+        public Food CreateFood(string typeOfFood, string nameOfFood,bool serveCold, List<string> listOfIngredients)
+        {
+            Food food;
+            if(typeOfFood.ToLower().Equals("appetizer"))
+            {
+                int id = 0;
+                //generator for ID needed
+                food = new Appetizer(id, nameOfFood, serveCold, listOfIngredients);
+            }
+            else if(typeOfFood.ToLower().Equals("secondmeal"))
+            {
+                int id = 0;
+                string userInput = Console.ReadLine();
+
+                bool needToCook;
+                if(bool.TryParse(userInput, out needToCook)){ }
+                else
+                {
+                    throw new Exception("ParseError");
+                }
+
+
+                TimeSpan timeToPrepare; //00:00:00 format
+                userInput = Console.ReadLine();
+                string[] timeAfterSplit = userInput.Split(":");
+                if(timeAfterSplit.Length<3 || timeAfterSplit.Length>3)
+                {
+                    throw new Exception("ParseError");
+                }
+                else
+                {
+                    int hours=0;
+                    int minutes=1;
+                    int seconds=2;
+
+                    if(int.TryParse(timeAfterSplit[hours], out hours)) { }
+                    else
+                    {
+                        throw new Exception("ParseError");
+                    }
+
+                    if(int.TryParse(timeAfterSplit[minutes], out minutes)) { }
+                    else
+                    {
+                        throw new Exception("ParseError");
+                    }
+
+                    if (int.TryParse(timeAfterSplit[seconds], out seconds)) { }
+                    else
+                    {
+                        throw new Exception("ParseError");
+                    }
+                }
+
+
+                List<string> listOfSpices;
+
+                food =new SecondMeal(id, nameOfFood, serveCold, listOfIngredients, )
+            }
+
+            return null;
+        }
+
+        public void AddFood(Food food)
+        {
+            ListOfFoods.Add(food);
+        }
     }
 }
