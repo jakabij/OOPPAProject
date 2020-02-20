aa06527f5757629b777048856f890791326c1f4e
jakabij <jakabistvanj@gmail.com>
2020-02-20 10:28:45 +0100

    Food (abstract class) added

--- OOPPAProject/Food.cs
+++ OOPPAProject/Food.cs
@@ -4,7 +4,19 @@ using System.Text;
 
 namespace OOPPAProject
 {
-    class Food
+    public abstract class Food
     {
+        public int Id { get; set; }
+        public string NameOfFood { get; set; }
+        public bool ServeCold { get; set; }
+        public List<string> ListOfIngredients { get; set; }
+
+        public Food(int id, string nameOfFood, bool serveCold, List<string> listOfingredients)
+        {
+            Id = id;
+            NameOfFood = nameOfFood;
+            ServeCold = serveCold;
+            ListOfIngredients = listOfingredients;
+        }
     }
 }
